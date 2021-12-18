510a0da16f36bd07d56e5cce97d97995f34c56f2
Nitay Shoshana <nitay2020@gmail.com>
2021-12-18 12:28:49 +0200

    WIP: bomb class

--- bomb.py
+++ bomb.py
@@ -1,1 +1,16 @@
 
+from game_display import GameDisplay
+from location import Location
+
+
+class Bomb(object):
+    def __init__(self, x, y, radius, timer) -> None:
+        self.location = Location(x, y)
+        self.radius = radius
+        self.timer = timer
+    
+    def draw_blast(self, gd :GameDisplay):
+        gd.draw_cell(self.x)
+
+    def detonate(self, gd :GameDisplay):
+        for i in range(self.radius):
