b7d69ea4a4fff236c3de9363418e3e15fe4adade
Nitay Shoshana <nitay2020@gmail.com>
2021-12-21 00:15:36 +0200

    add: SnakeNode docstring

--- snake_node.py
+++ snake_node.py
@@ -2,11 +2,17 @@
 from location import Location
 
 class SnakeNode(object):
+    """
+    A node in the snake which is a linked list. See snake.py.
+    """
     def __init__(self, x :int, y :int, next :object, previous :object) -> None:
+        """
+        initiates a SnakeNode instance
+        :param x:
+        :param y:
+        :param next: the next node in linked list chain
+        :param previous: the previous node in linked list chain
+        """
         self.location = Location(x, y)
         self.next = next
         self.previous = previous
-
-
-    def set_location(self, new_x, new_y):
-        self.location = Location(new_x, new_y)
