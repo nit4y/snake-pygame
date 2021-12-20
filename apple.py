702582fb319f5b0decc21d785dd3891036b11e5e
Nitay Shoshana <nitay2020@gmail.com>
2021-12-21 00:11:32 +0200

    add: apple class docstrings

--- apple.py
+++ apple.py
@@ -4,9 +4,22 @@ from location import Location
 from consts import GREEN
 
 class Apple(object):
+    """
+    A class representing an apple in the game
+    """
     def __init__(self, x: int, y: int, score_value :int) -> None:
+        """
+        initiates an Apple object instance
+        :param x:
+        :param y:
+        :param score_value: the score the player gets when eating this apple
+        """
         self.score_value = score_value
         self.location = Location(x, y)
     
     def draw(self, gd :GameDisplay) -> None:
+        """
+        draws the apple on the board
+        :param gd: GameDisplay instance to access drawing api
+        """
         gd.draw_cell(self.location.x, self.location.y, GREEN)
